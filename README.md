1412eaaef3ac53df9819d79aaf2ea8bbe156bc25
Krystian Laubach <krystian.laubach@wrenkitchens.com>
2021-07-29 12:16:39 +0100

    Add About page and React Routing

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # React Task Tracker UI
 
 I am not an author of this app. I have created it for learning purpose based on the [React JS Crash Course 2021](https://youtu.be/w7ejDZ8SWv8) with small amendments. 
-All credits to [Brad Traversy](https://github.com/bradtraversy) from [Traversy Media](https://www.youtube.com/c/TraversyMedia/featured).
 
+All credits to [Brad Traversy](https://github.com/bradtraversy) from [Traversy Media](https://www.youtube.com/c/TraversyMedia/featured).
 Original repository [Task Tracker App](https://github.com/bradtraversy/react-crash-2021).
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). 
 I have also used [React Date Picker](https://github.com/Hacker0x01/react-datepicker) component and  [Moment.js](https://momentjs.com/) library.
+
+My modifications:
+
+- Use [React Date Picker](https://github.com/Hacker0x01/react-datepicker) instead of text field for Task Date Time
+- Use [Moment.js](https://momentjs.com/) for date time formatting
+- Sort tasks by date
+- Display error above input instead of displaying alert popup
 
 ## Usage
 
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import AddTask from './components/AddTask';
 import Tasks from './components/Tasks';
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
     const [showAddTask, setShowAddTask] = useState(false);
@@ -43,11 +46,19 @@ function App() {
     };
 
     return (
-        <div className='container'>
-            <Header showAddTask={ showAddTask } onToggleAddTask={ toggleAddTask } />
-            { showAddTask && <AddTask onSaveTask={ saveTask } /> }
-            <Tasks tasks={ tasks } onToggle={ toggleReminder } onDelete={ deleteTask } />
-        </div>
+        <Router>
+            <div className='container'>
+                <Header showAddTask={ showAddTask } onToggleAddTask={ toggleAddTask } />
+                <Route path ='/' exact render={() => (
+                    <div>
+                        { showAddTask && <AddTask onSaveTask={ saveTask } /> }
+                        <Tasks tasks={ tasks } onToggle={ toggleReminder } onDelete={ deleteTask } />
+                        <Footer />
+                    </div>
+                )} />
+                <Route path='/about' component={ About }/>
+            </div>
+        </Router>
     );
 }
 
